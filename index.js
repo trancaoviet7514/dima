53f9dfb6b437456380f1e804ad06edd7d6714774
VietTC <viettc@vng.com.vn>
2019-08-09 17:46:20 +0700

    done upload image

--- index.js
+++ index.js
@@ -4,43 +4,10 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
-const request = require('request')
-const apiKey = 'd300988803d4e61ba56059b9b6dc1836'
-var mysql = require('mysql');
- 
-var conn = mysql.createConnection({
-  database: 'mall',
-  host: "localhost",
-  user: "root",
-  password: "221447514aA#"
-});
- 
-conn.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+const router = require('./router');
 
-app.get('/', function (req, res) {
-  res.render('index')
-})
-
-app.post('/', function (req, res) {
-    let city = req.body.city
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-    request(url, function (err, response, body) {
-        if(err){
-            res.render('index', {weather: null, error: 'Error, please try again'});
-        } else {
-            let weather = JSON.parse(body)
-            if(weather.main == undefined){
-                res.render('index', {weather: null, error: 'Error, please try again'});
-            } else {
-                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-                res.render('index', {weather: weatherText, error: null});
-            }
-        }
-    });
-  })
+app.use('/', router)
+app.use('/upload', router)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
