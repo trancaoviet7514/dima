8d75d94388b7679e0fd1ebf2c1a4a62f0e0e24de
trancaoviet7514 <trancaoviet7514@gmail.com>
2019-08-24 23:04:41 +0700

    finish host online in heroku

--- router.js
+++ router.js
@@ -1,53 +1,24 @@
 const express = require('express');
-// const app = express();
 const router = express.Router();
 const multer = require('multer');
 const upload = multer({dest: __dirname + '/public/image'});
-var mysql = require('mysql');
-var sharp = require('sharp')
 
-// var conn = mysql.createConnection({
-//     database: 'test',
-//     host: "localhost",
-//     user: "root",
-//     password: "ty0918936373"
-//   });
-   
-//   conn.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//   });
-
-  const { Pool, Client } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'mall',
-  password: '221447514aA#',
-  port: 5432,
-})
-// pool.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
+const {Client} = require('pg')
 const client = new Client({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'mall',
-  password: '221447514aA#',
+  user: 'yujeunvafgnwbm',
+  host: 'ec2-23-21-186-85.compute-1.amazonaws.com',
+  database: 'd65ufhdinrm8ir',
+  password: '540742461b897e1d6e2d342f4cd4e6b055c7abf55db717767ba87aa89968ee2a',
   port: 5432,
+  ssl: true
 })
 client.connect()
-client.query('SELECT * from book', (err, res) => {
-  console.log(err, res)
-})
-
-router.get('/', function (req, res) {
-    var getAllBookStr = 'select * from book'
-    client.query(getAllBookStr, function(err, results) {
-        if (err) throw err;
-        res.render('index', {products: results.rows})
-    });
+router.get('/', function (req, res) {    
+  var getAllBookStr = 'select * from book'
+  client.query(getAllBookStr, function(err, results) {
+      if (err) throw err;
+      res.render('index', {products: results.rows})
+  });
 })
 
 router.get('/upload', function (req, res) {
@@ -72,7 +43,8 @@ router.post('/upload', upload.single('photo'), (req, res) => {
                     N\'${req.body.price}\',
                     \'${req.file.filename}\',
                     \'${req.body.phone}\')`
-    conn.query(insertStr, function(err, results) {
+    
+    client.query(insertStr, function(err, results) {      
         if (err) throw err;
         console.log("Insert a record!");
     });
